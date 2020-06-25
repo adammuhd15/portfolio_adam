636770f263bcf12ec0952e00cb555092c78abd87
Adam Muhammad <adammuhammad@Adams-MacBook-Pro.local>
2020-06-25 16:44:47 +0800

    Second commit

--- JS/portfolio-draft4.js
+++ JS/portfolio-draft4.js
@@ -1,12 +1,185 @@
 var wOne = document.getElementById("profile-pic");
+var wTwo = document.getElementById("uni");
+var wThree = document.getElementById("clg");
+
+var pOne = document.getElementById("rail");
+var pTwo = document.getElementById("cross");
+var pThree = document.getElementById("beam");
+var pFour = document.getElementById("food");
+
+var xOne = document.getElementById("theatre");
+var xTwo = document.getElementById("obs");
+var xThree = document.getElementById("canada");
+var xFour = document.getElementById("zoo");
+var xFive = document.getElementById("orphan");
+var xSix = document.getElementById("camp");
+
+var yOne = document.getElementById("about-content");
+var yTwo = document.getElementById("work-section");
+var yThree = document.getElementById("skills");
+var yFour = document.getElementById("skills2");
+var yFive = document.getElementById("btnPjc");
 
 window.addEventListener("scroll", function(){
-    if (window.scrollY >= wOne.offsetTop-400) {
+    if (window.scrollY >= wOne.offsetTop-600) {
         wOne.style.opacity = 1;
-        wOne.style.animation = "fadeInBox 0.5s";
+        wOne.style.animation = "fadeInBox 2s";
     }
     else {
         wOne.style.opacity = 0;
         wOne.style.animation = "fadeOutBox 0.5s";
+    }
+
+    if (window.scrollY >= wTwo.offsetTop-600) {
+        wTwo.style.opacity = 1;
+        wTwo.style.animation = "fadeInBox 2s";
+    }
+    else {
+        wTwo.style.opacity = 0;
+        wTwo.style.animation = "fadeOutBox 0.5s";
+    }
+
+    if (window.scrollY >= wThree.offsetTop-600) {
+        wThree.style.opacity = 1;
+        wThree.style.animation = "fadeInBox 2s";
+    }
+    else {
+        wThree.style.opacity = 0;
+        wThree.style.animation = "fadeOutBox 0.5s";
+    }
+
+    if (window.scrollY >= pOne.offsetTop-600) {
+        pOne.style.opacity = 1;
+        pOne.style.animation = "fadeInBox 2s";
+    }
+    else {
+        pOne.style.opacity = 0;
+        pOne.style.animation = "fadeOutBox 0.5s";
+    }
+
+    if (window.scrollY >= pTwo.offsetTop-600) {
+        pTwo.style.opacity = 1;
+        pTwo.style.animation = "fadeInBox 2s";
+    }
+    else {
+        pTwo.style.opacity = 0;
+        pTwo.style.animation = "fadeOutBox 0.5s";
+    }
+
+    if (window.scrollY >= pThree.offsetTop-600) {
+        pThree.style.opacity = 1;
+        pThree.style.animation = "fadeInBox 2s";
+    }
+    else {
+        pThree.style.opacity = 0;
+        pThree.style.animation = "fadeOutBox 0.5s";
+    }
+
+    if (window.scrollY >= pFour.offsetTop-600) {
+        pFour.style.opacity = 1;
+        pFour.style.animation = "fadeInBox 2s";
+    }
+    else {
+        pFour.style.opacity = 0;
+        pFour.style.animation = "fadeOutBox 0.5s";
+    }
+
+    if (window.scrollY >= xOne.offsetTop-600) {
+        xOne.style.opacity = 1;
+        xOne.style.animation = "fadeInBox 2s";
+    }
+    else {
+        xOne.style.opacity = 0;
+        xOne.style.animation = "fadeOutBox 0.5s";
+    }
+
+    if (window.scrollY >= xTwo.offsetTop-600) {
+        xTwo.style.opacity = 1;
+        xTwo.style.animation = "fadeInBox 2s";
+    }
+    else {
+        xTwo.style.opacity = 0;
+        xTwo.style.animation = "fadeOutBox 0.5s";
+    }
+
+    if (window.scrollY >= xThree.offsetTop-600) {
+        xThree.style.opacity = 1;
+        xThree.style.animation = "fadeInBox 2s";
+    }
+    else {
+        xThree.style.opacity = 0;
+        xThree.style.animation = "fadeOutBox 0.5s";
+    }
+
+    if (window.scrollY >= xFour.offsetTop-600) {
+        xFour.style.opacity = 1;
+        xFour.style.animation = "fadeInBox 2s";
+    }
+    else {
+        xFour.style.opacity = 0;
+        xFour.style.animation = "fadeOutBox 0.5s";
+    }
+
+    if (window.scrollY >= xFive.offsetTop-600) {
+        xFive.style.opacity = 1;
+        xFive.style.animation = "fadeInBox 2s";
+    }
+    else {
+        xFive.style.opacity = 0;
+        xFive.style.animation = "fadeOutBox 0.5s";
+    }
+
+    if (window.scrollY >= xSix.offsetTop-600) {
+        xSix.style.opacity = 1;
+        xSix.style.animation = "fadeInBox 2s";
+    }
+    else {
+        xSix.style.opacity = 0;
+        xSix.style.animation = "fadeOutBox 0.5s";
+    }
+
+    if (window.scrollY >= yOne.offsetTop-600) {
+        yOne.style.opacity = 1;
+        yOne.style.animation = "fadeInBox 2s";
+    }
+    else {
+        yOne.style.opacity = 0;
+        yOne.style.animation = "fadeOutBox 0.5s";
+    }
+
+    if (window.scrollY >= yTwo.offsetTop-600) {
+        yTwo.style.opacity = 1;
+        yTwo.style.animation = "fadeInBox 2s";
+    }
+    else {
+        yTwo.style.opacity = 0;
+        yTwo.style.animation = "fadeOutBox 0.5s";
+    }
+
+    if (window.scrollY >= yThree.offsetTop-800) {
+        yThree.style.opacity = 1;
+        yThree.style.animation = "fadeInBox 2s";
+    }
+    else {
+        yThree.style.opacity = 0;
+        yThree.style.animation = "fadeOutBox 0.5s";
+    }
+
+    if (window.scrollY >= yFour.offsetTop-800) {
+        yFour.style.opacity = 1;
+        yFour.style.animation = "fadeInBox 2s";
+    }
+    else {
+        yFour.style.opacity = 0;
+        yFour.style.animation = "fadeOutBox 0.5s";
+    }
+
+    if (window.scrollY >= yFive.offsetTop-800) {
+        yFive.style.opacity = 1;
+        yFive.style.animation = "fadeInBox 2s";
+    }
+    else {
+        yFive.style.opacity = 0;
+        yFive.style.animation = "fadeOutBox 0.5s";
     }
 });
